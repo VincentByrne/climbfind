e6f8ef701fdd982ca4369c7384465ab9138d59e2
Vincent Byrne <149702125+VincentByrne@users.noreply.github.com>
2025-03-17 20:18:00 +0000

    Climbfind version 6 - API introduction
    Introduces an API and exspands on the core tests.

---
+++ climbfind 0.6/src/models/mongo/location.js
@@ -0,0 +1,17 @@
+import Mongoose from "mongoose";
+
+const { Schema } = Mongoose;
+
+const locationSchema = new Schema({
+  title: String,
+  description: String,
+  category: String,
+  latitude: Number,
+  longitude: Number,
+  userid: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+export const Location = Mongoose.model("Location", locationSchema);
